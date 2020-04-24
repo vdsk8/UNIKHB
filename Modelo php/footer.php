d9836d08dbe8871a14326f4cc9b82a4ea1de5460
vdsk8 <vdsk8@yahoo.com.br>
2020-04-24 15:49:39 +0200

    Add files via upload

---
+++ Modelo php/footer.php
@@ -0,0 +1,19 @@
+	<!-- SCRIPTS
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+
+    <!-- jQuery -->
+    <script src="<?php bloginfo('template_url'); ?>vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php bloginfo('template_url'); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="<?php bloginfo('template_url'); ?>js/agency.min.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
